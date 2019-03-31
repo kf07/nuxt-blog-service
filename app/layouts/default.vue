2ab6acc7fc435f7670b5a0e58c0d65ab3ce0a07f
kango furusu <furusu0715@gmail.com>
2019-03-31 23:20:16 +0900

    header作成

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,20 @@
 <template>
   <div>
-    <nuxt />
+    <TheHeader />
+    <div class="wrapper">
+      <nuxt class="container" />
+    </div>
   </div>
 </template>
 
 <style>
 
 </style>
+<script>
+  import TheHeader from "../components/TheHeader";
+  export default {
+    components: {
+      TheHeader
+    }
+  }
+</script>
